3b8de2e356a4a2de50d48cdbddd2de39ad78fd5c
rinku176 <rinkubijoy2000@gmail.com>
2024-07-04 16:05:08 +0100

    TutorialUpdates | 16:04 | 4.7.24

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,16 +11,6 @@ public class GameManager : MonoBehaviour
 
     public static int levelsCompleted = 0;
 
-    public int aaa;
-    public float bbb;
-    public string ccc;
-    public bool ddd;
-
-    public int aa;
-    public float bb;
-    public string cc;
-    public bool dd;
-
     public bool check;
     public bool isread;
     public bool issave;
@@ -31,6 +21,7 @@ public class GameManager : MonoBehaviour
      * When a new Game Manager is created, it always replaces the old one.
      * This means that a New Game has been started.
      */
+
     private void Awake()
     {
         if (instance != null)
@@ -44,37 +35,12 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        // 尝试加载游戏数据
+        
         gameData = dataManager.LoadGameData();
-        if (gameData == null)
-        {
-            // 如果没有找到保存的数据，初始化新的数据
-            gameData = new GameData
-            {
-                a = 4,
-                b = 11.2f,
-                c = "ssss",
-                d = true
-            };
-        }
+        
         dataManager.SaveGameData(gameData);
     }
-    private void Update()
-    {
-        debugnum();
-        if (check&&issave )
-        {
-            gameData.a = aaa;
-            gameData.b = bbb;
-            gameData.c = ccc;
-            gameData.d = ddd;
-        }
-        else if(check && isread)
-        {
-            UpdateGameData(aaa, bbb, ccc, ddd);
-        }
-    }
-
+    
     // 示例：更新游戏数据的方法
     public void UpdateGameData(int newScore, float newVolume, string newName,bool isss)
     {
@@ -88,12 +54,5 @@ public class GameManager : MonoBehaviour
     {
         // 在应用退出时保存游戏数据
         dataManager.SaveGameData(gameData);
-    }
-    void debugnum()
-    {
-        aa = gameData.a;
-        bb = gameData.b;
-        cc = gameData.c;
-        dd = gameData.d;    
     }
 }
