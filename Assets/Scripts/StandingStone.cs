ba05fad8f818099b0da37ce32928052b177927dd
remy5623 <guitarguru767@gmail.com>
2024-07-10 19:05:38 +0100

    Remy input manager01 (#103)
    * Converted CameraPan to InputManager | 9:43 | 10.07.24
    
    * Moved BuildSystem and LevelManager inputs to InputManager | 11:43 | 10.07.24
    
    * Fixed a NullRef check | 11:46 | 10.07.24
    
    -- Camera.main can be null when exiting Playmode
    
    * Moved StandingStone input to InputManager | 13:44 | 10.07.24
    
    * Removed Pause Menu Input
    
    -- Input now pauses through the Time System
    
    * Cancelled Other Input On Camera Pan | 18:30 | 10.07.24
    
    Input Priority is now:
    
    1. Camera
    2. Building System
    3. Radius Visualisation
    
    * Further Merge Fixes | 19:04 | 10.07.24
    
    -- Moved StandingStone input back to Input Manager
    -- Removed debug print statements

--- Assets/Scripts/StandingStone.cs
+++ Assets/Scripts/StandingStone.cs
@@ -1,27 +1,16 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class StandingStone : MonoBehaviour
 {
-    [SerializeField]
-    InputActionAsset actionAsset;
-
     public Kelpie kelpie;
     
     public Cailleach cailleach;
-
-    InputAction placeAction;
-    InputAction tapLocation;
 
     Terrainsystem TS1;
 
     [SerializeField] GameObject IslandToChange;
 
     [SerializeField] StandingStonPrefabPopUp StandingStonePrefab;
-
-    // This function reference is necessary for callback registering/deregistering to work properly
-    Action<InputAction.CallbackContext> click;
 
     private void Start()
     {
@@ -31,12 +20,6 @@ public class StandingStone : MonoBehaviour
             kelpie.gameObject.SetActive(false);
         if(cailleach)
             cailleach.gameObject.SetActive(false);
-
-        placeAction = actionAsset.FindAction("click");
-        click = ctx => Interact();
-        placeAction.performed += click;
-
-        tapLocation = actionAsset.FindAction("PanCamera");
 
         RaycastHit hit;
         if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, LayerMask.GetMask("Terrain")))
@@ -52,16 +35,6 @@ public class StandingStone : MonoBehaviour
         {
             StandingStonPrefabPopUp standingStoneUI = Instantiate(StandingStonePrefab.gameObject).GetComponent<StandingStonPrefabPopUp>();
             standingStoneUI.SetStandingStoneReference(this);
-        }
-    }
-
-    void Interact()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(tapLocation.ReadValue<UnityEngine.Vector2>());
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit) && hit.collider.gameObject.tag == "StandingStone")
-        {
-            OpenStandingStone();
         }
     }
 
@@ -102,24 +75,14 @@ public class StandingStone : MonoBehaviour
                 {
                     if (building.resourceData == building.oldresourceData)
                     {
-                        building.VeilChangeActivate();
-/*                        newResourceData = 
-                            Destroy
-                            Instantiate*/
-                        
+                        building.VeilChangeActivate(); 
                     }
                     else
                     {
                         building.VeilChangeDeactivate();
-                        
                     }
                 }
             }
         }
-    }
-
-    private void OnDestroy()
-    {
-        placeAction.performed -= click;
     }
 }
