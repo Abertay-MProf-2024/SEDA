10241f4a9ab222a08d96f5001202d781568628f9
rinku176 <rinkubijoy2000@gmail.com>
2024-07-10 17:52:49 +0100

    TilesFreedWhenSwitched | 15:51 | 10.7.24

--- Assets/Scripts/StandingStonPrefabPopUp.cs
+++ Assets/Scripts/StandingStonPrefabPopUp.cs
@@ -2,6 +2,19 @@ using UnityEngine;
 
 public class StandingStonPrefabPopUp : MonoBehaviour
 {
+    StandingStone standingStone;
+
+    public void SetStandingStoneReference(StandingStone stone)
+    {
+        standingStone = stone;
+    }
+
+    public void SwitchIsland()
+    {
+        standingStone.VeilSwitch();
+        CloseMenu();
+    }
+
     public void CloseMenu()
     {
         Destroy(gameObject);
