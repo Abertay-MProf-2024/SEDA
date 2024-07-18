d7cfed5c8384994629b33f361aed6943768ace7e
remy5623 <guitarguru767@gmail.com>
2024-07-18 15:44:55 +0100

    Remy archie zoom sound fix15 (#158)
    Changed Wind Sound Logic | 15:43 | 18.07.24

--- Assets/Scripts/MusicManager/AudioZoomController.cs
+++ Assets/Scripts/MusicManager/AudioZoomController.cs
@@ -1,7 +1,5 @@
 using UnityEngine;
-using System.Collections;
 using UnityEngine.Audio;
-using UnityEngine.SceneManagement;
 
 public class AudioZoomController : MonoBehaviour
 {
@@ -22,8 +20,6 @@ public class AudioZoomController : MonoBehaviour
 
     private void Awake()
     {
-        SceneManager.sceneLoaded += OnSceneLoaded;
-
         // Initialize AudioSources
         sfxSource = gameObject.AddComponent<AudioSource>();
         sfxSource.clip = windSound;
@@ -32,78 +28,13 @@ public class AudioZoomController : MonoBehaviour
         sfxSource.outputAudioMixerGroup = mixerGroupSFX;
     }
 
-    void Update()
-    {
-        if (orthoCam != null && orthoCam.orthographic)
-        {
-            float zoomLevel = orthoCam.orthographicSize;
-
-            float volume1 = Mathf.InverseLerp(maxZoom - 1, minZoom, zoomLevel);  // »º³åÇø¼ä
-            float volume2 = Mathf.InverseLerp(minZoom + 3, maxZoom, zoomLevel);
-
-            StartCoroutine(FadeAudioSource.StartFade(musicSource, fadeDuration, volume1));
-
-            if (volume2 >= 0.95f && !isMusic2AtMaxVolume)
-            {
-                isMusic2AtMaxVolume = true;
-                music2OriginalVolume = volume2;
-                if (music2FadeCoroutine != null)
-                {
-                    StopCoroutine(music2FadeCoroutine);
-                }
-                music2FadeCoroutine = StartCoroutine(WaitAndLowerVolume(sfxSource, 5.0f, volume2 * 0.4f));
-            }
-            else if (volume2 < 0.95f)
-            {
-                isMusic2AtMaxVolume = false;
-                if (music2FadeCoroutine != null)
-                {
-                    StopCoroutine(music2FadeCoroutine);
-                }
-                StartCoroutine(FadeAudioSource.StartFade(sfxSource, fadeDuration, volume2));
-            }
-        }
-    }
-
-    private IEnumerator WaitAndLowerVolume(AudioSource audioSource, float waitTime, float targetVolume)
-    {
-        yield return new WaitForSeconds(waitTime);
-        StartCoroutine(FadeAudioSource.StartFade(audioSource, fadeDuration, targetVolume));
-    }
-
-    private void OnDestroy()
-    {
-        SceneManager.sceneLoaded -= OnSceneLoaded;
-    }
-
-    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    public void PlayWindSound()
     {
         sfxSource.Play();
-
-        orthoCam = FindAnyObjectByType<Camera>();
-
-        InputManager cameraPan;
-        if (orthoCam != null && (cameraPan = orthoCam.GetComponent<InputManager>()))
-        {
-            minZoom = cameraPan.minZoomDistance;
-            maxZoom = cameraPan.maxZoomDistance;
-        }
     }
-}
 
-public static class FadeAudioSource
-{
-    public static IEnumerator StartFade(AudioSource audioSource, float duration, float targetVolume)
+    public void StopWindSound()
     {
-        float currentTime = 0;
-        float startVolume = audioSource.volume;
-
-        while (currentTime < duration)
-        {
-            currentTime += Time.deltaTime;
-            audioSource.volume = Mathf.Lerp(startVolume, targetVolume, currentTime / duration);
-            yield return null;
-        }
-        yield break;
+        sfxSource.Stop();
     }
 }
