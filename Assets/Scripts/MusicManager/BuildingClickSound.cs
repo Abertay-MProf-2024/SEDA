fea32a1fbfa03946574d97c4f0b817a881885394
remy5623 <guitarguru767@gmail.com>
2024-08-01 05:05:18 -0600

    Added Sound VFX to Cows and Sheep | 12:03 | 01.08.24 (#252)

--- Assets/Scripts/MusicManager/BuildingClickSound.cs
+++ Assets/Scripts/MusicManager/BuildingClickSound.cs
@@ -1,8 +1,11 @@
+using System.Collections;
 using UnityEngine;
 
 public class BuildingClickSound : MonoBehaviour
 {
     public AudioClip clickSound;
+    [SerializeField] Transform vfxLocation;
+    [SerializeField] GameObject vfxForSound;
     private AudioSource audioSource;
 
     void Start()
@@ -21,10 +24,24 @@ public class BuildingClickSound : MonoBehaviour
         if (audioSource && clickSound != null)
         {
             audioSource.PlayOneShot(clickSound);
+
+            if (vfxForSound)
+            {
+                GameObject vfx = Instantiate(vfxForSound, vfxLocation.position, Quaternion.Euler(0, 0, 0));
+                vfx.transform.parent = vfxLocation.transform;
+                StartCoroutine(DestroyVFX(vfx));
+            }
         }
         else
         {
             Debug.LogWarning("ClickSound is null in " + gameObject.name);
         }
+    }
+
+    IEnumerator DestroyVFX(GameObject objectToDestroy)
+    {
+        yield return new WaitForSeconds(clickSound.length);
+
+        Destroy(objectToDestroy);
     }
 }
